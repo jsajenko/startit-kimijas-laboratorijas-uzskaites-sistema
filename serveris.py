c6ee2a28eaf10ce602f638a32d591819ace5ebfb
JulijaMazure <mazure.skola@gmail.com>
2020-12-29 09:49:29 +0000

    pievienoti aprēķini

--- serveris.py
+++ serveris.py
@@ -11,6 +11,13 @@ def index():
 @app.route('/sveiki')
 def sveiki():
   return('Nav rīts!')
+  
+  @app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1, sk2):
+  sk1=int(sk1)
+  sk2=int(sk2)
+  reizinajums = sk1*sk2
+  return str(reizinajums)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
