ebf3c19b399f9d32f79bce43bf71135631a56127
JulijaMazure <mazure.skola@gmail.com>
2020-12-29 09:37:40 +0000

    pievienota rote seviki

--- serveris.py
+++ serveris.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Labrīt!"
-
+@app.route('/sveiki')
+def index():
+  return('Nav rīts!')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
